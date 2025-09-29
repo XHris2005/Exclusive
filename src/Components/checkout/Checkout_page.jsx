7a36b5d2a07bf61263d18404f277e681966262d7
Chris <christoph3eze@gmail.com>
2025-09-29 23:10:19 +0100

    integrated paystack payment

--- src/Components/checkout/Checkout_page.jsx
+++ src/Components/checkout/Checkout_page.jsx
@@ -2,13 +2,32 @@ import React from "react";
 import './Checkout_page.css'
 import { useState, useContext } from "react";
 import { CartContext } from "../../Context/CartContext";
+import { PaystackButton } from 'react-paystack'
 import all_products from "../assets/All_Product";
 import Bkash from '../assets/Bkash.png'
 import Visa from '../assets/Visa.png'
 import Mastercard from '../assets/Mastercard.png'
 import Nagad from '../assets/Nagad.png'
 const Checkout_page = () => {
+    const publicKey = 'pk_test_0f3f4393a5ed9ca332b99218c225825f341441c8'
     const {getTotalAmount,cartItems,addToCart, removeFromCart} = useContext(CartContext)
+    const [firstName, setFirstName] = useState('')
+    const [companyName, setCompanyName] = useState('')
+    const [streetAddress, setstreetAddress] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const [email, setEmail] = useState('')
+    const config = {
+        email,
+        amount : getTotalAmount() * 100,
+        metadata : {
+            firstName,
+            phoneNumber
+        },
+        publicKey,
+        text : 'pay now',
+        onSuccess: (reference)=> alert(`payment complete ${reference}`),
+        onClose:()=>alert('are you sure')
+    }
     return (  
         <>
         <div className="checkout_page">
@@ -20,13 +39,16 @@ const Checkout_page = () => {
                     <h2>Billing Details</h2>
                     <form action="">
                         <label htmlFor="first_name">first Name</label>
-                        <input type="text" id="first_name" />
+                        <input type="text" id="first_name" value={firstName}
+                            onChange={(e)=> setFirstName(e.target.value)}/>
 
                         <label htmlFor="company_name">Company Name</label>
-                        <input type="text" id="campany_name" />
+                        <input type="text" id="campany_name" value={companyName}
+                            onChange={(e)=> setCompanyName(e.target.value)}/>
 
                         <label htmlFor="street_address">Street Address</label>
-                        <input type="text" id="street_address" />
+                        <input type="text" id="street_address" value={streetAddress}
+                            onChange={(e)=> setstreetAddress(e.target.value)} />
 
                         <label htmlFor="apartment_floor">Apartment, floor (optional)</label>
                         <input type="text" id="apartment_floor" />
@@ -35,10 +57,12 @@ const Checkout_page = () => {
                         <input type="text" id="town_city" />
 
                         <label htmlFor="phone_number">Phone Number</label>
-                        <input type="no" id="phone_number" />
+                        <input type="no" id="phone_number" value={phoneNumber}
+                            onChange={(e)=> setPhoneNumber(e.target.value)}/>
 
                         <label htmlFor="email_address">Email Address</label>
-                        <input type="text" email="email_address" />
+                        <input type="email" email="email_address" value={email}
+                            onChange={(e)=> setEmail(e.target.value)}/>
                     </form>
                 </div>
                 <div className="checkout_panel">
@@ -93,7 +117,7 @@ const Checkout_page = () => {
                             <input type="text" placeholder="Coupon Code"/>
                             <button>Apply Coupon</button>
                         </div>
-                        <button className="place_order">Place order</button>
+                        <PaystackButton {...config} className="place_order">Place order</PaystackButton>
                     </div>
                 </div>
             </div>
